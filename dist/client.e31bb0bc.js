cc1f6f3e6ea119720cfa05374335a10fcc27d4e8
noisyneuron <hello@playdo.io>
2020-09-05 00:44:17 -0400

    rebuilt

--- dist/client.e31bb0bc.js
+++ dist/client.e31bb0bc.js
@@ -28694,7 +28694,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53094" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62645" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
